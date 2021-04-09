db1808927e2a7c87554a388041f92685c81c3d3e
McDonaldsOfficial <correonormal321@gmail.com>
2021-04-08 23:14:38 -0400

    Chat de grupos finalizado

--- public/js/main.js
+++ public/js/main.js
@@ -5,19 +5,74 @@ $(function(){
     const btn_login = $('#btnLogin');
     const txt_nombre =$('#txtNombre');
     const txt_id=$('#txtId');
+    const chat_container = $('#chatContainer');
+    const login_container = $('#main');
+    const errores_login = $('#errores');
+    const btn_crear_grupo = $('#btnCrearGrupo');
+    const notificaciones = $('#notificaciones');
+    const txt_nombre_grupo = $('#txtNombreGrupo');
+    const txt_id_grupo = $('#txtIdGrupo');
+    const txt_id_usuarios = $('#txtIdUsuarios');
+    const txt_mensaje_grupo = $('#txtMensajeGrupo');
+    const txt_id_mensaje_grupo = $('#txtIDMensajeGrupo');
+    const btn_mensaje_grupo = $('#btnMensaje');
 
+    /*Eventos de los elementos */
     btn_login.click(()=>{
-        socket.emit('e_login',{
+        socket.emit('login',{
             nombre:txt_nombre.val(),
             id:txt_id.val()
         },(error)=>{
             if(error){
                 /*TODO:Adjuntar mensaje de error */
                 console.log('ERROR, EL ID DE USUARIO YA EXISTE');
+                errores_login.html('ERROR, EL ID DE USUARIO YA EXISTE');
             }else{
                 /*TODO:Pasar a la siguiente página de alguna forma */
                 console.log('LOGEADO');
+                login_container.hide();
+                chat_container.show();
             }
         });
     });
+
+    btn_crear_grupo.click(()=>{
+        let id_usuarios = txt_id_usuarios.val().split(',');
+        if(id_usuarios[0]==='') id_usuarios[0]=txt_id.val();
+        else id_usuarios.push(txt_id.val());
+        let nuevo_grupo={
+            nombre:txt_nombre_grupo.val(),
+            id:txt_id_grupo.val(),
+            usuarios:id_usuarios
+        }        
+        console.log(nuevo_grupo);
+
+        socket.emit('nuevoGrupo',(nuevo_grupo),(error,msj)=>{
+            if (error) {
+                console.log('ERROR AL CREAR EL GRUPO ',msj);
+            } 
+        });        
+    });
+    
+    btn_mensaje_grupo.click(()=>{
+        let data = {
+            nombre:txt_nombre.val(),
+            id:txt_id_mensaje_grupo.val(),
+            mensaje:txt_mensaje_grupo.val()            
+        }
+        socket.emit('mensajeGrupo',(data),(error,msj)=>{
+           if(error) console.log(msj);
+        });
+
+    });
+
+    /*Eventos recibidos de los sockets*/
+    socket.on('nuevoGrupo',data=>{
+        notificaciones.append(data);
+    });
+
+    socket.on('mensajeGrupo',data=>{
+        notificaciones.append(data);
+    });
+
 });
--- sockets.js
+++ sockets.js
@@ -4,20 +4,58 @@ module.exports = function (io){
     let usuarios = {
 
     }
+    let grupos = {
+
+    }
 
     io.on('connection',socket=>{
         console.log('Nueva conexión');
-        socket.on('e_login',(data,callBack)=>{
-            console.log(data);
+        socket.on('login',(data,callBack)=>{      
             if(data.id in usuarios){
                 callBack(true);
             }else{
                 callBack(false);
                 socket.id = data.id;
                 socket.nombre = data.nombre;
+                socket.grupos = [];
                 usuarios[socket.id] = socket;
             }
         });
 
+        socket.on('nuevoGrupo',(data,callBack)=>{
+            console.log(data);
+            if(data.id in grupos){
+                callBack(true,'El id del grupo ya existe');
+                return;
+            }
+            let existen_usuarios = data.usuarios.every((item)=>item in usuarios);
+            if(!existen_usuarios){
+                callBack(true,'Uno de los id de usuarios no existe')
+                return;
+            }                        
+            grupos[data.id]=data;
+            socket.grupos.push(data.id);
+            callBack(false,'');                        
+            /*REDUCIR A UNA SOLA LINEA(? */
+            for(let usuario in usuarios){
+                if(data.usuarios.indexOf(usuario)!==-1){
+                    usuarios[usuario].emit('nuevoGrupo',`<div> <i>Nuevo grupo creado:</i> <b> ${data.nombre} </b> </div> <br/>`)
+                }
+            }            
+            //socket.broadcast.emit('chat:typing',data);
+        });
+
+        socket.on('mensajeGrupo',(data,callBack)=>{
+            if(!data.id in grupos){
+                callBack(true,'El grupo no existe');
+                return;
+            }
+            let usuarios_en_grupo = grupos[data.id].usuarios;
+            for(let usuario in usuarios){
+                if(usuarios_en_grupo.indexOf(usuario)!==-1){
+                    usuarios[usuario].emit('mensajeGrupo',`<div> <i>${data.nombre}</i> <b> ${data.mensaje} </b> </div> <br/>`);
+                }
+            } 
+        });
     });
 }
